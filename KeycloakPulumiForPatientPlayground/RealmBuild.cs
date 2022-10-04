7d7eb531ef7e21ed6d0b6eecb8e6255bb458a58a
Paul DeVito <pdevito3@gmail.com>
2022-10-03 21:45:45 -0400

    fix: auth scopes

--- KeycloakPulumiForPatientPlayground/RealmBuild.cs
+++ KeycloakPulumiForPatientPlayground/RealmBuild.cs
@@ -18,55 +18,55 @@ class RealmBuild : Stack
             RememberMe = true,
             EditUsernameAllowed = true
         });
-        var recipemanagementScope = ScopeFactory.CreateScope(realm.Id, "recipe_management");
+        var patientmanagementScope = ScopeFactory.CreateScope(realm.Id, "patient_management");
         
         var patientManagementPostmanMachineClient = ClientFactory.CreateClientCredentialsFlowClient(realm.Id,
-            "recipe_management.postman.machine", 
+            "patient_management.postman.machine", 
             "974d6f71-d41b-4601-9a7a-a33081f84682", 
             "PatientManagement Postman Machine",
             "https://oauth.pstmn.io");
-        patientManagementPostmanMachineClient.ExtendDefaultScopes(recipemanagementScope.Name);
-        patientManagementPostmanMachineClient.AddAudienceMapper("recipe_management");
+        patientManagementPostmanMachineClient.ExtendDefaultScopes(patientmanagementScope.Name);
+        patientManagementPostmanMachineClient.AddAudienceMapper("patient_management");
         
         var patientManagementPostmanCodeClient = ClientFactory.CreateCodeFlowClient(realm.Id,
-            "recipe_management.postman.code", 
+            "patient_management.postman.code", 
             "974d6f71-d41b-4601-9a7a-a33081f84680", 
             "PatientManagement Postman Code",
             "https://oauth.pstmn.io",
             redirectUris: null,
             webOrigins: null
             );
-        patientManagementPostmanCodeClient.ExtendDefaultScopes(recipemanagementScope.Name);
-        patientManagementPostmanCodeClient.AddAudienceMapper("recipe_management");
+        patientManagementPostmanCodeClient.ExtendDefaultScopes(patientmanagementScope.Name);
+        patientManagementPostmanCodeClient.AddAudienceMapper("patient_management");
         
         var patientManagementSwaggerClient = ClientFactory.CreateCodeFlowClient(realm.Id,
-            "recipe_management.swagger", 
+            "patient_management.swagger", 
             "974d6f71-d41b-4601-9a7a-a33081f80687", 
             "PatientManagement Swagger",
             "https://localhost:5875",
             redirectUris: null,
             webOrigins: null
             );
-        patientManagementSwaggerClient.ExtendDefaultScopes(recipemanagementScope.Name);
-        patientManagementSwaggerClient.AddAudienceMapper("recipe_management");
+        patientManagementSwaggerClient.ExtendDefaultScopes(patientmanagementScope.Name);
+        patientManagementSwaggerClient.AddAudienceMapper("patient_management");
         
         var patientManagementNextClient = ClientFactory.CreateCodeFlowClient(realm.Id,
-            "recipe_management.next", 
+            "patient_management.next", 
             "974d6f71-d41b-4601-9a7a-a33081f82188", 
             "PatientManagement Next",
-            "https://localhost:8511",
+            "http://localhost:8511",
             redirectUris: new InputList<string>() 
                 {
-                "https://localhost:8511/*",
+                "http://localhost:8511/*",
                 },
             webOrigins: new InputList<string>() 
                 {
                 "https://localhost:5875",
-                "https://localhost:8511",
+                "http://localhost:8511",
                 }
             );
-        patientManagementNextClient.ExtendDefaultScopes(recipemanagementScope.Name);
-        patientManagementNextClient.AddAudienceMapper("recipe_management");
+        patientManagementNextClient.ExtendDefaultScopes(patientmanagementScope.Name);
+        patientManagementNextClient.AddAudienceMapper("patient_management");
         
         var bob = new User("bob", new UserArgs
         {
